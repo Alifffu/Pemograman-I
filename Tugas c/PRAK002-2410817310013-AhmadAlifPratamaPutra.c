cc70ee6e20ec241f45b01d4aec2834bada496fbc
Alifffu <2410817310013@mhs.ulm.ac.id>
2024-12-28 22:17:29 +0800

    Add files via upload

---
+++ Tugas c/PRAK002-2410817310013-AhmadAlifPratamaPutra.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main (){
+    printf("Selamat Pagi, Ahmad ALif Pratama Putra\n");
+    printf("Selamat Siang, Ahmad Alif Pratama Putra\n");
+    printf("Selamat Malam, Ahmad Alif Pratama Putra");
+}
