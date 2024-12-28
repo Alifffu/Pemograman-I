7fc8bd908246f118751002321b3cc91535a946fc
Alifffu <2410817310013@mhs.ulm.ac.id>
2024-12-28 22:16:05 +0800

    Delete PRAK002-2410817310013-AhmadAlifPratamaPutra.c

--- PRAK002-2410817310013-AhmadAlifPratamaPutra.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main (){
-    printf("Selamat Pagi, Ahmad ALif Pratama Putra\n");
-    printf("Selamat Siang, Ahmad Alif Pratama Putra\n");
-    printf("Selamat Malam, Ahmad Alif Pratama Putra");
-}
