4462adc50977be67971eb2d0f1f5bd42818698b8
Alifffu <2410817310013@mhs.ulm.ac.id>
2024-12-28 22:18:28 +0800

    Delete Tugas c directory

--- Tugas c/PRAK004-2410817310013-AhmadAlifPratamaPutra.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main (){
-    printf("##############################\n");
-    printf("#                            #\n");
-    printf("#  Ahmad Alif Pratama Putra  #\n");
-    printf("#       2410817310013        #\n");
-    printf("#                            #\n");
-    printf("##############################  ");
-}
